2ab85f058a653d87a2fc390bb72296f7ef38247c
AmnesiaBeing <727184139@qq.com>
2020-01-28 14:10:28 +0800

    合并

--- bootloader/boot.c
+++ bootloader/boot.c
@@ -4,11 +4,14 @@
 EFI_STATUS
 efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *systab)
 {
+    SIMPLE_TEXT_OUTPUT_INTERFACE *conout;
+
     //gnu-efi的要求，加载系统表systable
     InitializeLib(image_handle, systab);
 
     //调用API在屏幕上输出Hello World!
-    Print(L"Hello World!\n\r");
+    conout = systab->ConOut;
+	uefi_call_wrapper(conout->OutputString, 2, conout, (CHAR16 *)L"Hello World!\n\r");
 
     //按任意键后退出模拟器
     WaitForSingleEvent(systab->ConIn->WaitForKey, 0);
